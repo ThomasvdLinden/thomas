c63451f653c23c341d1c4b18cd565feb3ca76387
ThomasvdLinden <94378329+ThomasvdLinden@users.noreply.github.com>
2021-12-06 13:17:10 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,21 @@
-// JavaScript Document
+
+
+var deButton = document.querySelector("header ul:first-of-type li:last-of-type");
+
+deButton.addEventListener("click", verander);
+
+function verander() {
+  let menu = document.querySelector("header ul:nth-of-type(2)");
+
+  menu.classList.toggle("show");
+}
+
+
+var ButtonGrijs = document.querySelector("#buttons a:nth-of-type(1) p");
+
+
+
+
+
+
+var ButtonWit = document.querySelector("#buttons a:nth-of-type(2) p");
